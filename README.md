55049428bf81183a830f582f87269044d8723fc3
Mara Kim <mara@autochthe.net>
2016-05-24 14:38:12 -0500

    Add basic elements

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-console
 Simple react console emulator
+
+Inspired by [chrisdone/jquery-console](https://github.com/chrisdone/jquery-console).
--- src/react-console.jsx
+++ src/react-console.jsx
@@ -1,1 +1,18 @@
 "use strict";
+
+import react from 'react';
+import './react-console.css';
+
+module.exports = {
+	render: function() {
+		<div className="react-console-container">
+		<textarea className="react-console-typer"></textarea>
+		{this.props.welcomeMessage?
+			<div className="react-console-message react-console-welcome">
+				this.props.welcomeMessage
+			</div>
+			: null
+		}
+		</div>
+	}
+};
