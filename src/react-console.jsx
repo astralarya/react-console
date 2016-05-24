1886fe080d5f23822b2c820ef94d64f76a7c5a8e
Mara Kim <mara@autochthe.net>
2016-05-24 16:19:04 -0500

    Add textarea properties

--- src/react-console.jsx
+++ src/react-console.jsx
@@ -6,7 +6,12 @@ import './react-console.css';
 module.exports = React.createClass({
 	render: function() {
 		return <div className="react-console-container">
-			<textarea className="react-console-typer"></textarea>
+			<textarea className="react-console-typer"
+				autocomplete="off"
+				autocorrect="off"
+				autocapitalize="off"
+				spellcheck="false"
+			></textarea>
 			{this.props.welcomeMessage?
 				<div className="react-console-message react-console-welcome">
 					this.props.welcomeMessage
