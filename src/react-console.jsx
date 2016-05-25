de88d9fbc68fed0d216fd82a1e45b9ed525d7e66
Mara Kim <mara@autochthe.net>
2016-05-25 13:02:37 -0500

    Log trace display

--- src/react-console.jsx
+++ src/react-console.jsx
@@ -4,6 +4,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './react-console.css';
 
+let ConsolePrompt = React.createClass({
+	render: function() {
+		return <div className="react-console-prompt-box">
+			<span className="react-console-prompt-label">{this.props.label}</span>
+			<span className="react-console-prompt">{this.props.value}</span>
+		</div>;
+	}
+});
+
+let ConsoleMessage = React.createClass({
+	render: function() {
+		return <div className={"react-console-message " + this.props.success?"react-console-message-success":"react-console-message-failure"}>
+			{this.props.value}
+		</div>;
+	}
+});
+
+
 module.exports = React.createClass({
 	getInitialState: function() {
 		return {
@@ -11,6 +29,7 @@ module.exports = React.createClass({
 			column: 0,
 			history: [],
 			ringn: 0,
+			log: [],
 		};
 	},
 	getDefaultProps: function() {
@@ -43,6 +62,15 @@ module.exports = React.createClass({
 				</div>
 				: null
 			}
+			{this.state.log.map( (val, idx) => {
+				return [
+					<ConsolePrompt label={this.props.promptLabel} value={val.command} />,
+					(val.message?
+						<ConsoleMessage success={val.message.success} value={val.message.value} />
+						:null
+					)
+				];
+			})}
 		</div>;
 	}
 });
