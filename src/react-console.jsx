92a7730bb8649b3ccbe29dbafeb0e865f3173bf9
Mara Kim <mara@autochthe.net>
2016-05-24 21:53:14 -0500

    Add state

--- src/react-console.jsx
+++ src/react-console.jsx
@@ -1,16 +1,41 @@
 "use strict";
 
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './react-console.css';
 
 module.exports = React.createClass({
+	getInitialState: function() {
+		return {
+			promptText: '',
+			column: 0,
+			history: [],
+			ringn: 0,
+		};
+	},
+	getDefaultProps: function() {
+		return {
+			promptLabel: '> ',
+		};
+	},
+	componentDidMount() {
+		if(this.props.autofocus) {
+			ReactDOM.findDOMNode(this.refs.typer).focus();
+		}
+	},
 	render: function() {
 		return <div className="react-console-container">
-			<textarea className="react-console-typer"
+			<textarea ref="typer"
+				className="react-console-typer"
 				autocomplete="off"
 				autocorrect="off"
 				autocapitalize="off"
 				spellcheck="false"
+				style={{
+					position: 'absolute',
+					top: 0,
+					left: '-9999px',
+				}}
 			></textarea>
 			{this.props.welcomeMessage?
 				<div className="react-console-message react-console-welcome">
