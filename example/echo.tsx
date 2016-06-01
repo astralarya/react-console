507a71d4292378231da42d2c901ce984a9cd3a24
Mara Kim <mara@autochthe.net>
2016-06-01 18:48:10 -0500

    Document and add example of promptLabel as a function

--- example/echo.tsx
+++ example/echo.tsx
@@ -4,17 +4,32 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Console from '../src/react-console.tsx';
 
-class EchoConsole extends React.Component<{},{}> {
+interface EchoConsoleState {
+	count: number;
+}
+class EchoConsole extends React.Component<{},EchoConsoleState> {
+	constructor(props: {}) {
+		super(props);
+		this.state = {
+			count: 0,
+		};
+	}
 	child: {
 		console?: Console,
 	} = {};
 	echo = (text: string) => {
 		this.child.console.log(text);
-		this.child.console.return();
+		this.setState({
+			count: this.state.count+1,
+		}, this.child.console.return);
+	}
+	promptLabel = () => {
+		return this.state.count + "> ";
 	}
 	render() {
 		return <Console ref={ref => this.child.console = ref}
 			handler={this.echo}
+			promptLabel={this.promptLabel}
 			autofocus={true}
 		/>;
 	}
