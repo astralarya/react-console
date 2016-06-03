b1d5944d2ae34e2e81c9bed3e80e2b894c9411d0
Mara Kim <mara@autochthe.net>
2016-06-03 02:26:34 -0500

    More detailed server status

--- server.js
+++ server.js
@@ -2,13 +2,18 @@
 
 "use strict";
 
+const PORT=8080;
+
+
+// Webpack build server
 process.argv.push('--watch');
 require('./node_modules/.bin/webpack');
 
+// Express web server
 var express = require('express');
 var app = express();
-
-const PORT=8080;
+var os = require('os');
+var ifaces = os.networkInterfaces();
 
 app.use('/', express.static(__dirname + '/example/dist'));
 app.get('/', function(req, res) {
@@ -16,6 +21,21 @@ app.get('/', function(req, res) {
 });
 
 app.listen(PORT, function() {
-	console.log("Server listening on: http://localhost:%s", PORT);
+	serverStatus();
+	setTimeout(function() {
+		serverStatus();
+	}, 10000);
 });
 
+function serverStatus() { 
+	console.log("Server listening on:")
+	console.log("http://localhost:%s", PORT);
+
+	Object.keys(ifaces).forEach(function (ifname) {
+		ifaces[ifname].forEach(function (iface) {
+			if ('IPv4' === iface.family && iface.internal === false) {
+				console.log("http://%s:%s", iface.address, PORT);
+			}
+		});
+	});
+}
