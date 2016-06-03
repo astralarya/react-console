9aaf62f1936b929f2ed7210d8d690ad5d1d930a9
Mara Kim <mara@autochthe.net>
2016-06-03 02:41:36 -0500

    report server status after build

--- server.js
+++ server.js
@@ -6,8 +6,15 @@ const PORT=8080;
 
 
 // Webpack build server
-process.argv.push('--watch');
 require('./node_modules/.bin/webpack');
+var webpack = require('webpack');
+var config = require('./webpack.config.js');
+var compiler = webpack(require('./webpack.config.js'));
+
+var first_wave = config.length - 1;
+compiler.watch({},function(err,stats) {
+	if(!err && --first_wave < 0) serverStatus();
+});
 
 // Express web server
 var express = require('express');
@@ -22,12 +29,9 @@ app.get('/', function(req, res) {
 
 app.listen(PORT, function() {
 	serverStatus();
-	setTimeout(function() {
-		serverStatus();
-	}, 10000);
 });
 
-function serverStatus() { 
+function serverStatus() {
 	console.log("Server listening on:")
 	console.log("http://localhost:%s", PORT);
 
