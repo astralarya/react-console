69be1c9db03b46fd0bbbe44761425e3f4f56e061
Mara Kim <mara@autochthe.net>
2016-06-03 00:12:53 -0500

    Compile library version

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,12 @@ var path = require('path');
 var fs = require('fs');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-let config = {
+let bundle = {
 	context: __dirname + "/src",
 	entry: './react-console.tsx',
 	output: {
 		path: __dirname + '/dist',
-		filename: 'react-console.js',
+		filename: 'react-console.bundle.js',
 		library: "Console",
 		libraryTarget: "var",
 	},
@@ -35,15 +35,7 @@ let config = {
 	],
 };
 
-
-let production = Object.assign({},config, {
-	output: {
-		path: __dirname + '/dist',
-		filename: 'react-console.min.js',
-		library: "Console",
-		libraryTarget: "var",
-	},
-	plugins: [
+let production_plugins = [
 		new ExtractTextPlugin("react-console.min.css"),
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': '"production"'
@@ -51,10 +43,52 @@ let production = Object.assign({},config, {
 		new webpack.optimize.UglifyJsPlugin(),
 		new webpack.optimize.OccurenceOrderPlugin(),
 		new webpack.optimize.DedupePlugin()
-	]
+]
+
+
+let bundle_min = Object.assign({},bundle, {
+	output: {
+		path: __dirname + '/dist',
+		filename: 'react-console.bundle.min.js',
+		library: "Console",
+		libraryTarget: "var",
+	},
+	plugins: production_plugins,
 });
 
-let development = Object.assign({},config, {
+let dist = Object.assign({},bundle, {
+	output: {
+		path: __dirname + '/dist',
+		filename: 'react-console.js',
+		library: "Console",
+		libraryTarget: "var",
+	},
+	externals: {
+		"react": "React",
+		"react-dom": "ReactDOM",
+	},
+});
+
+let dist_min = Object.assign({},dist, {
+	output: {
+		path: __dirname + '/dist',
+		filename: 'react-console.min.js',
+		library: "Console",
+		libraryTarget: "var",
+	},
+	plugins: production_plugins,
+});
+
+let lib = Object.assign({},dist, {
+	output: {
+		path: __dirname + '/lib',
+		filename: 'react-console.js',
+		library: "Console",
+		libraryTarget: "commonjs",
+	},
+});
+
+let development = Object.assign({},bundle, {
 	entry: '../example/echo.tsx',
 	output: {
 		path: __dirname + '/example/dist',
@@ -64,4 +98,4 @@ let development = Object.assign({},config, {
 	},
 });
 
-module.exports = [ config, production, development ];
+module.exports = [ bundle, bundle_min, dist, dist_min, lib, development ];
