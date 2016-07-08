0dd0ea5db8fbc6164d629c7379627318fd71df8b
Mara Kim <mara@autochthe.net>
2016-07-08 00:50:14 -0500

    Add welcomeMessage to example

--- example/src/example.tsx
+++ example/src/example.tsx
@@ -31,6 +31,7 @@ class EchoConsole extends React.Component<{},EchoConsoleState> {
 		return <Console ref={ref => this.child.console = ref}
 			handler={this.echo}
 			promptLabel={this.promptLabel}
+			welcomeMessage={"Welcome to the react-console demo!"}
 			autofocus={true}
 		/>;
 	}
