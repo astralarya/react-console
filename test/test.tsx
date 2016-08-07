6bad1358ae9fbffc535aa3629a3c87a1669ed1de
Mara Kim <mara@autochthe.net>
2016-08-07 12:39:09 -0500

    Rename tests

--- test/test.tsx
+++ test/test.tsx
@@ -8,11 +8,11 @@ const ConsoleMessage = require('exports?ConsoleMessage!../src/react-console.tsx'
 
 describe('<ConsoleMessage />', function() {
 	describe('type: ', function () {
-		it('Has CSS class `react-console-message` even when null', function() {
+		it('Has class `react-console-message` when type=null', function() {
 			var wrapper = enzyme.shallow(<ConsoleMessage />);
 			assert(wrapper.hasClass('react-console-message'), 'Missing class `react-console-message`');
 		});
-		it('Adds CSS class `react-console-message-foo` when == \'foo\'', function() {
+		it('Has class `react-console-message react-console-message-foo` when type=\'foo\'', function() {
 			var wrapper = enzyme.shallow(<ConsoleMessage type='foo'/>);
 			assert(wrapper.hasClass('react-console-message-foo'), 'Missing class `react-console-message-foo`');
 			assert(wrapper.hasClass('react-console-message'), 'Missing class `react-console-message`');
