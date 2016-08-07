f2b819f2f6cf6104490166447993ab227c801872
Mara Kim <mara@autochthe.net>
2016-08-06 23:37:46 -0500

    Remove artifactual import

--- test/test.tsx
+++ test/test.tsx
@@ -3,7 +3,6 @@
 import * as React from 'react';
 import { assert, expect } from 'chai';
 import * as enzyme from 'enzyme';
-import * as rewire from 'rewire';
 
 const ConsoleMessage = require('exports?ConsoleMessage!../src/react-console.tsx');
 
