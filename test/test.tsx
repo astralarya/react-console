bb0d7d2f2a3a4d11cb4ebe2745bb51b6d5b0c1bb
Mara Kim <mara@autochthe.net>
2016-08-06 22:27:08 -0500

    Add tests for type property of ConsoleMessage

--- test/test.tsx
+++ test/test.tsx
@@ -1,7 +1,7 @@
 'use strict';
 
 import * as React from 'react';
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import * as enzyme from 'enzyme';
 import * as rewire from 'rewire';
 
@@ -9,8 +9,14 @@ const ConsoleMessage = require('exports?ConsoleMessage!../src/react-console.tsx'
 
 describe('<ConsoleMessage />', function() {
 	describe('type: ', function () {
-		it('Adds CSS class `react-console-message`', function() {
+		it('Has CSS class `react-console-message` even when null', function() {
 			var wrapper = enzyme.shallow(<ConsoleMessage />);
+			assert(wrapper.hasClass('react-console-message'), 'Missing class `react-console-message`');
+		});
+		it('Adds CSS class `react-console-message-foo` when == \'foo\'', function() {
+			var wrapper = enzyme.shallow(<ConsoleMessage type='foo'/>);
+			assert(wrapper.hasClass('react-console-message-foo'), 'Missing class `react-console-message-foo`');
+			assert(wrapper.hasClass('react-console-message'), 'Missing class `react-console-message`');
 		});
 	});
 });
