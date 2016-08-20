d6366ccaed47366599c284d431454d83bbcec06a
Mara Kim <mara@autochthe.net>
2016-08-20 02:02:56 -0500

    Add more description

--- docs/example/src/example.tsx
+++ docs/example/src/example.tsx
@@ -31,7 +31,7 @@ class EchoConsole extends React.Component<{},EchoConsoleState> {
 		return <Console ref={ref => this.child.console = ref}
 			handler={this.echo}
 			promptLabel={this.promptLabel}
-			welcomeMessage={"Welcome to the react-console demo!"}
+			welcomeMessage={"Welcome to the react-console demo!\nThis is an example of a simple echo console."}
 			autofocus={true}
 		/>;
 	}
